7193f16fb0bf2f8fa14218f843cc63d33c21ff2a
Abrar Alolyan <abrar-ol@users.noreply.github.com>
2024-05-08 18:04:54 +0300

    Allow player to double jump when pick up a fruit

---
+++ scripts/green_fruit.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+# delete coin on body entered
+func _on_body_entered(body):
+	queue_free()
+	body._change_max_jump(2)
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,30 +3,28 @@ extends CharacterBody2D
 
 const SPEED = 120.0
 const JUMP_VELOCITY = -300.0
-const  MAX_JUMPS = 2
+var  max_jump = 1
 var jump_counter = 0
 var isDoubleJump = false
 
-
 @onready var animated_sprite = $AnimatedSprite2D
-
-
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _change_max_jump(maxJump:int):
+	max_jump=maxJump
 
 func _physics_process(delta):
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	if is_on_floor() || jump_counter>MAX_JUMPS: 
+	if is_on_floor() || jump_counter>max_jump: 
 		jump_counter = 0
 		isDoubleJump=false
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and jump_counter < MAX_JUMPS:
+	if Input.is_action_just_pressed("jump") and jump_counter < max_jump:
 		jump_counter += 1
 		velocity.y = JUMP_VELOCITY
 
@@ -37,11 +35,13 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
- 	# flip player right and left
+ 	# flip player right or left
 	if direction < 0 :
 		animated_sprite.flip_h = true
-	else :
+	elif  direction > 0:
 		animated_sprite.flip_h = false
+
+		
 		
 	# Handle Movement animation
 	if is_on_floor():
@@ -59,3 +59,4 @@ func _physics_process(delta):
 				
 
 	move_and_slide()
+
